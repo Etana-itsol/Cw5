251d18cab4ec8e0e1e4c038705036f7d5ded6483
s18725 <61834255+s18725@users.noreply.github.com>
2020-04-12 16:27:20 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -19,7 +19,7 @@ begin tran
 		return
 	end
 
-	declare @oldEnrollment int =( select IdEnrollment from Studies s, Enrollment e where e.IdStudy= s.IdStudy and Semester = @semester and @Name = Name);
+	declare @oldEnrollment int =( select IdEnrollment from Studies s, Enrollment e where e.IdStudy= s.IdStudy and Semester = @semester and @Name = Name)
 	declare @Id int=(select idenrollment from Enrollment e join Studies s on s.IdStudy=e.IdStudy where Semester=@Semester+1 and name =@name)
 	if @Id is null
 	begin
@@ -28,7 +28,7 @@ begin tran
 		print'Dodano Enrollment'
 	end
 
-	update student set IdEnrollment = @Id where IdEnrollment=@oldEnrollment;
+	update student set IdEnrollment = @Id where IdEnrollment=@oldEnrollment
 	print'promocja studentow'
 	commit
 end
@@ -42,38 +42,38 @@ as
 begin
 	begin tran
 
-	declare @IdStudies int = (select IdStudy from studies where name=@Studies);
-	declare @NumberExist int = (select count(*) from Student where IndexNumber=@indexNumber);
+	declare @IdStudies int = (select IdStudy from studies where name=@Studies)
+	declare @NumberExist int = (select count(*) from Student where IndexNumber=@indexNumber)
 	if @NumberExist != 0
 	begin
-		print 'Student o takim ID juz istnieje';
-		rollback;
-		return;
-	end;
+		print 'Student o takim ID juz istnieje'
+		rollback
+		return
+	end
 	if @IdStudies is null
 	begin
-		print 'Nie ma takich studiow';
-		rollback;
-		return;
-	end;
+		print 'Nie ma takich studiow'
+		rollback
+		return
+	end
 	declare @IdEnrollment int = (select top 1 IdEnrollment
-								from Enrollment where Semester=1 and IdStudy=@IdStudies
-								order by StartDate desc);
+					from Enrollment where Semester=1 and IdStudy=@IdStudies
+					order by StartDate desc)
 	if @IdEnrollment is null
 	begin
-		declare @currentDate Date = (SELECT CONVERT(char(10), GetDate(),126));
-		declare @maxIdEnrollment int = (select max(IdEnrollment) from Enrollment);
+		declare @currentDate Date = (SELECT CONVERT(char(10), GetDate(),126))
+		declare @maxIdEnrollment int = (select max(IdEnrollment) from Enrollment)
 
 		insert into Enrollment (IdEnrollment, Semester,IdStudy,StartDate)
-		values ((@maxIdEnrollment+1), 1, @IdStudies, @currentDate);
+		values ((@maxIdEnrollment+1), 1, @IdStudies, @currentDate)
 
-		Print 'Dodano element';
-		set @IdEnrollment = @maxIdEnrollment +1;
-	end;
+		Print 'Dodano element'
+		set @IdEnrollment = @maxIdEnrollment +1
+	end
 	insert into Student (IndexNumber,FirstName,LastName,BirthDate,IdEnrollment) 
-	values (@indexNumber, @FirstName, @LastName, @BirthDate, @IdEnrollment);
-	commit;
-end;
+	values (@indexNumber, @FirstName, @LastName, @BirthDate, @IdEnrollment)
+	commit
+end
 
 		
 exec EnrollStudent  '12334', 'Mateusz', 'Pawlak', '1998-03-03','IT'
